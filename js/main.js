d482f3b37fac3ffa3ecf5e9171146f26cf99b1de
yqio61 <156813321+yqio61@users.noreply.github.com>
2025-03-27 16:02:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,18 @@ $('.slider-carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 1000,
     arrows: false,
   });
 
+  $(document).ready(function() {
+    $('.lang-link').on('click', function(e) {
+      e.preventDefault();  
+      $('.lang-link').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
   
   $(document).ready(function() {
     $('.info-item-btn').click(function() {
@@ -16,18 +23,13 @@ $('.slider-carousel').slick({
     });
 });
 
+$(document).ready(function() {
+  $('.info-add-btn').click(function() {
+      $(this).toggleClass('rotate-180');
+      $(this).closest('.info-add').find('.info-add-text').toggleClass('display-none');
+  });
+});
 
-// $(function() {
-//   $('.burger').on('click', function() {
-//       $(this).toggleClass('active');
-//       $('body').toggleClass('stop-scroll');
-//       $('.menu').toggleClass('visible');
-//   })
-//   $('.menu-link, .menu-list').on('click', function() {
-//       $('.burger').removeClass('active');
-//       $('.menu').removeClass('visible');
-//   })
-// })
 
 
 $(function() {
@@ -36,7 +38,6 @@ $(function() {
     $('body').toggleClass('stop-scroll');
     $('.menu').toggleClass('visible');
     
-    // Добавление/удаление эффекта размытия для main
     $('.main').css('filter', function(_, currentValue) {
       return currentValue === 'blur(10px)' ? '' : 'blur(10px)';
     });
@@ -46,7 +47,21 @@ $(function() {
     $('.burger').removeClass('active');
     $('.menu').removeClass('visible');
     
-    // Убираем размытие при закрытии меню
     $('.main').css('filter', '');
+  });
+});
+
+
+$(function() {
+  $("ul.tabs__caption").on("click", "li:not(.active)", function() {
+      $(this)
+          .addClass("active")
+          .siblings()
+          .removeClass("active")
+          .closest("div.tabs")
+          .find("div.tabs__content")
+          .removeClass("active")
+          .eq($(this).index())
+          .addClass("active");
   });
 });
